93490fb9abea208ba2fbd29798d3d68e07080f36
Emilio Larrambebere <emilio.larrambebere@ncr.com>
2016-06-12 08:11:52 +0100

    Added models and basic controllers

---
+++ API/API/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public Category Category { get; set; }
+    }
+}
